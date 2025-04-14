51187241f900f172774ccb6845116a198d04426d
DayeneS <dayene-santos@hotmail.com>
2025-04-14 10:45:19 -0300

    Aula 04 - Operadores de Atribuição

--- src/Introducao/Aula04Operadores4Atribuicao.java
+++ src/Introducao/Aula04Operadores4Atribuicao.java
@@ -1,5 +1,25 @@
 package Introducao;
 
 public class Aula04Operadores4Atribuicao {
+    public static void main(String[] args) {
 
+        // = += -= *= /= %=
+
+        double bonus = 1800; // 1800
+        bonus += 1000; // resultado -> 2800
+        bonus -= 1000; // resultado -> 1800
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+
+        System.out.println(bonus);
+        System.out.println("------------------");
+
+        // ++ --
+        int contador = 0;
+        contador +=1; // contador = contador + 1;
+        contador ++; // mesmo resultado que a operação acima
+        contador --;
+        System.out.println(contador);
+    }
 }
