68ee5964d259d338f908b0671405e880f44b80c4
DayeneS <dayene-santos@hotmail.com>
2025-04-14 12:28:12 -0300

    Aula 05 - Estruturas de Repetição While, do-while e For

---
+++ src/Introducao/Aula06EstruturasRepeticao1.java
@@ -0,0 +1,24 @@
+package Introducao;
+
+public class Aula06EstruturasRepeticao1 {
+    public static void main(String[] args) {
+
+        //while, do while, for
+        int count = 12;
+
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while " + ++count);
+        } while (count < 10);
+        System.out.println("----------------------");
+
+        count = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("For " + i);
+        }
+    }
+}
